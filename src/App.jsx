9d12ce01207edd846770e7a96c151dd845acbc76
Freddy Carrillo <freddycarrillotucto@gmail.com>
2024-10-15 22:08:42 -0500

    js added in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,87 @@
+import { format } from 'date-fns';
+import './App.css';
+import logo from './assets/img/logo.png';
+
 const App = () => {
+
+  let action1 = 'Login';
+  let action2 = 'Sign Up';
+  let usuariosActivos = 23;
+  let tagActiveLogin = true;
+  let currentDate = new Date();
+
+  let dataUser = {
+    nombre: 'Freddy',
+    edad: 26
+  };
+
+  let users = [
+    {
+      nombre: 'Freddy',
+      edad: 26
+    },
+    {
+      nombre: 'Daniel',
+      edad: 27
+    },
+    {
+      nombre: 'Joaquín',
+      edad: 28
+    }
+  ];
+
+  const {
+    edad
+  } = dataUser;
+
+  const [ usuarioFreddy, usuarioDaniel ] = users; 
+
+  console.log(currentDate);
+  console.log('edad',edad);
+  console.log('usuarioFreddy',usuarioFreddy);
+
   return (
-    <div>
-      <div style={{backgroundColor:'black', color: 'white', textAlign: 'center', padding: '20px', borderRadius: '20px', marginTop: '100px'}}>
-        <p>Hola mundo</p>
-      </div>
-      <div style={{backgroundColor:'black', color: 'white', textAlign: 'center', padding: '20px', borderRadius: '20px', marginTop: '100px'}}>
-        <p>Hola mundo</p>
-      </div>
-      <div style={{backgroundColor:'black', color: 'white', textAlign: 'center', padding: '20px', borderRadius: '20px', marginTop: '100px'}}>
-        <p style={{fontSize: '40px'}}>Hola mundo</p>
+    <div className="ctnGeneral">
+      <div className="ctnAuth">
+        
+        <div className="ctnLogo">
+          <img 
+            src={logo}
+            width="150"
+          />
+        </div>
+
+        <div style={{textAlign: 'center', marginBottom: '20px'}}>
+          {
+            format(currentDate, "dd-MM-yyyy")
+          }
+        </div>
+
+        <div className="ctnActions">
+          <div className="btnActive">
+            {action1}
+          </div>
+          <div className="btnActive">
+            {action2}
+          </div>
+        </div>
+
+        <div>
+          {
+            users.map((user)=>(
+              <div style={{display: 'flex', gap: '50px', padding: '20px'}}>
+                <div>{ user.nombre }</div>
+                <div>{ user.edad }</div>
+              </div>
+            ))
+          }
+        </div>
+
       </div>
     </div>
   )
+    
+
 }
 
 export default App;
